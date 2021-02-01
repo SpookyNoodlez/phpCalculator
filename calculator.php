71cacb5b253d2f8c8fc2bda22283c0a874bb5f93
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2021-02-02 01:50:26 +0200

    Form klart

---
+++ calculator.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>  
+    <head>
+    </head>
+    <body>
+
+        <form action="calculator.php" method="get">
+            <input type="number" name = x>
+            <input type="number" name = y>
+            <input type="submit" name="add" value = "+">
+            <input type="submit" name="sub" value = "-">
+            <input type="submit" name="mul" value = "*">
+            <input type="submit" name="div" value = "/">
+            <input type="submit" name="pow" value = "x^y">
+            <input type="submit" name="invert" value = "1/x">
+            <input type="submit" name="square" value = "x^2">
+            <input type="submit" name="squareRoot" value = "√x">
+
+        </form>
+
+
+        <?php
+            
+        ?>
+
+    </body>
+
+</html>
