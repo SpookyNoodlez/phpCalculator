8021f9e29810d877baf485880552f629a6a4e8f4
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2021-02-02 02:22:11 +0200

    Lade till funktioner för operationerna

--- calculator.php
+++ calculator.php
@@ -21,7 +21,72 @@
 
 
         <?php
-            
+            if(array_key_exists('add', $_POST)) { 
+                add(); 
+            }
+            if(array_key_exists('sub', $_POST)) { 
+                sub(); 
+            } 
+            if(array_key_exists('mul', $_POST)) { 
+                multiply(); 
+            } 
+            if(array_key_exists('div', $_POST)) { 
+                divide(); 
+            } 
+            if(array_key_exists('pow', $_POST)) { 
+                power(); 
+            } 
+            if(array_key_exists('invert', $_POST)) { 
+                invert(); 
+            } 
+            if(array_key_exists('square', $_POST)) { 
+                square(); 
+            } 
+            if(array_key_exists('squareRoot', $_POST)) { 
+                squareRoot(); 
+            } 
+
+
+            function add() {
+                $RESULT = $_GET["x"] + $_GET["y"];
+                #print somehow
+            }
+        
+            function sub() {
+                $RESULT = $_GET["x"] - $_GET["y"];
+                #print somehow
+            }
+
+            function multiply() {
+                $RESULT = $_GET["x"] * $_GET["y"];
+                #print somehow
+            }
+
+            function divide() {
+                $RESULT = $_GET["x"] / $_GET["y"];
+                #print somehow
+            }
+
+            function power() {
+                $RESULT = pow($_GET["x"], $_GET["y"]);
+                #print somehow
+            }
+
+            function invert() {
+                $RESULT = $_GET["x"] / 1;
+                #print somehow
+            }
+
+            function square() { #SUPPOSED TO BE HANDLED BY POWER
+                $RESULT = pow($_GET["x"], 2);
+                #print somehow
+            }
+
+            function squareRoot() {
+                $RESULT = sqrt($_GET["x"]);
+                #print somehow
+            }
+        
         ?>
 
     </body>
