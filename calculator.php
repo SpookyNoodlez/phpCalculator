819db83d1bf69c7efbdc8a549e6861c151da0a57
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2021-02-03 02:46:13 +0200

    Flyttade hanteringen av kvadrering till power funktionen

--- calculator.php
+++ calculator.php
@@ -39,34 +39,7 @@
 
 
         <?php
-            /*
-            if(array_key_exists('add', $_POST)) { 
-                add(); 
-            }
-            if(array_key_exists('sub', $_POST)) { 
-                sub(); 
-            } 
-            if(array_key_exists('mul', $_POST)) { 
-                multiply(); 
-            } 
-            if(array_key_exists('div', $_POST)) { 
-                divide(); 
-            } 
-            if(array_key_exists('pow', $_POST)) { 
-                power(); 
-            } 
-            if(array_key_exists('invert', $_POST)) { 
-                invert(); 
-            } 
-            if(array_key_exists('square', $_POST)) { 
-                square(); 
-            } 
-            if(array_key_exists('squareRoot', $_POST)) { 
-                squareRoot(); 
-            } 
-            */
-
-            //If submit button has been pressed call apropriate function
+            //If submit button has been pressed call appropriate function
             if(isset($_GET['submit'])){
                 $operation = $_GET['operation'];
                 switch($operation){
@@ -92,7 +65,7 @@
                         break;
                     case "x^y":
                         if(validate("$operation")){
-                            power();
+                            power(false);
                         }
                         break;
                     case "1/x":
@@ -102,7 +75,7 @@
                         break;
                     case "x^2":
                         if(validate("$operation")){
-                            square();
+                            power(true);
                         }
                         break;
                     case "√x":
@@ -161,49 +134,40 @@
 
             function add() {
                 $RESULT = $_GET["x"] + $_GET["y"];
-                #print somehow
                 echo $RESULT;
             }
         
             function sub() {
                 $RESULT = $_GET["x"] - $_GET["y"];
-                #print somehow
                 echo $RESULT;
             }
 
             function multiply() {
                 $RESULT = $_GET["x"] * $_GET["y"];
-                #print somehow
                 echo $RESULT;
             }
 
             function divide() {
                 $RESULT = $_GET["x"] / $_GET["y"];
-                #print somehow
                 echo $RESULT;
             }
 
-            function power() {
-                $RESULT = pow($_GET["x"], $_GET["y"]);
-                #print somehow
+            function power($square) { //pass ""true" to square
+                $yLocal = 2;
+                if($square == false){
+                    $yLocal = $_GET["y"];
+                }
+                $RESULT = pow($_GET["x"], $yLocal);
                 echo $RESULT;
             }
 
             function invert() {
                 $RESULT = 1 / $_GET["x"];
-                #print somehow
-                echo $RESULT;
-            }
-
-            function square() { #SUPPOSED TO BE HANDLED BY POWER
-                $RESULT = pow($_GET["x"], 2);
-                #print somehow
                 echo $RESULT;
             }
 
             function squareRoot() {
                 $RESULT = sqrt($_GET["x"]);
-                #print somehow
                 echo $RESULT;
             }
         
